59090266271d1510e5e30ac79588dca85f040fcc
神雕 <teasiu@qq.com>
2024-03-30 00:26:44 +0800

    Update recovery.sh

--- package/base-files/files/etc/recovery.sh
+++ package/base-files/files/etc/recovery.sh
@@ -12,11 +12,11 @@ fi
 flash_partition9() {
     mkdir -p /backup
 	mount -t ext4 /dev/mmcblk0p8 /backup
-	gunzip -c /backup/backup-$arch.gz | dd of=/dev/mmcblk0p9 bs=1024
+	gunzip -c /backup/backup-$arch.gz | dd of=/dev/mmcblk0p9 conv=fsync
 }
 echo "正在恢复系统刷机中，不要操作！不要断电！"
 flash_partition9
-
+resize2fs /dev/mmcblk0p9
 dd of=/dev/mmcblk0p2 if=/etc/bootargs/bootargs9-${arch}.bin bs=1024 count=1024 conv=fsync
 dd of=/dev/mmcblk0p5 if=/etc/bootargs/logo.img bs=1024 count=4096
 sleep 2
