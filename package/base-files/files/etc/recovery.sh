f8063b439d17b27bbaa1c616cce8067944dab62f
teasiu <teasiu@qq.com>
2025-03-20 23:38:03 +0800

    update-am40

--- package/base-files/files/etc/recovery.sh
+++ package/base-files/files/etc/recovery.sh
@@ -9,16 +9,16 @@ elif [ "$platform" = "armv7l" ]; then
     arch=32
 fi
 
-flash_partition9() {
+flash_partition4() {
     mkdir -p /backup
-	mount -t ext4 /dev/mmcblk0p8 /backup
-	gunzip -c /backup/backup-$arch.gz | dd of=/dev/mmcblk0p9 conv=fsync
+	mount -t ext4 /dev/mmcblk0p3 /backup
+	gunzip -c /backup/backup-$arch.gz | dd of=/dev/mmcblk0p4 conv=fsync
 }
 echo "正在恢复系统刷机中，不要操作！不要断电！"
-flash_partition9
-resize2fs /dev/mmcblk0p9
-dd of=/dev/mmcblk0p2 if=/etc/bootargs/bootargs9-${arch}.bin bs=1024 count=1024 conv=fsync
-dd of=/dev/mmcblk0p5 if=/etc/bootargs/logo.img bs=1024 count=4096
+flash_partition4
+resize2fs /dev/mmcblk0p4
+configfile="/boot/armbianEnv.txt"
+sed -e 's,rootdev=.*,rootdev=UUID=eb3496f9-e4fb-4311-8e5f-79a0239f71b2,g' -i $configfile
 sleep 2
 sync
 umount /backup
